9d070b5b88fb9b70e1f36bf5b7fad9f91a862055
Leonardo Saraiva <vyper@maneh.org>
2025-05-17 10:41:38 -0300

    chore(deploy): configures kamal deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = ActiveModel::Type::Boolean.new.cast(ENV.fetch('RAILS_CONFIG_FORCE_SSL', 'true'))
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
